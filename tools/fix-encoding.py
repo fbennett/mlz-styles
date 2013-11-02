0296272b6ced265197ff65e6ce4f4515e1bd5a34
Frank Bennett <biercenator@gmail.com>
2013-11-03 07:02:46 +0900

    Spruce up entity conversion script

--- tools/fix-encoding.py
+++ tools/fix-encoding.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
-import re
+import re,sys
 
 def replaceit (m):
     return unichr(int(m.group(1))).encode("utf-8")
 
-txt = open("mlz-horitsu-henshu-sha-konwa-kai.csl").read().encode("utf-8")
-txt = re.sub("&#([0-9]+);", replaceit, txt)
+def entityToChar (filename):
+    txt = open(filename).read().encode("utf-8")
+    txt = re.sub("&#([0-9]+);", replaceit, txt)
+    return txt
 
-print txt
+if len(sys.argv) == 2:
+    print entityToChar(sys.argv[1])
+else:
+    print "Script takes filename of a CSL style as sole argument"
